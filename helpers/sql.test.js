2d93e2872be825e9089b40a0e6659b475616ffee
rbooker <ryan.m.booker@gmail.com>
2022-01-18 11:54:51 -0800

    Add files via upload

---
+++ helpers/sql.test.js
@@ -0,0 +1,25 @@
+
+const { sqlForPartialUpdate } = require("./sql");
+
+
+describe("sqlForPartialUpdate", function () {
+  test("Works for a single item", function () {
+    const result = sqlForPartialUpdate(
+        { f1: "v1" },
+        { f1: "f1", fF2: "f2" });
+    expect(result).toEqual({
+      setCols: "\"f1\"=$1",
+      values: ["v1"],
+    });
+  });
+
+  test("Works for two items", function () {
+    const result = sqlForPartialUpdate(
+        { f1: "v1", jsF2: "v2" },
+        { jsF2: "f2" });
+    expect(result).toEqual({
+      setCols: "\"f1\"=$1, \"f2\"=$2",
+      values: ["v1", "v2"],
+    });
+  });
+});
---
+++ helpers/tokens.test.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+const { createToken } = require("./tokens");
+const { SECRET_KEY } = require("../config");
+
+describe("createToken", function () {
+  test("Works for non-admins", function () {
+    const token = createToken({ username: "test", is_admin: false });
+    const payload = jwt.verify(token, SECRET_KEY);
+    expect(payload).toEqual({
+      iat: expect.any(Number),
+      username: "test",
+      isAdmin: false,
+    });
+  });
+
+  test("Works for admins", function () {
+    const token = createToken({ username: "test", isAdmin: true });
+    const payload = jwt.verify(token, SECRET_KEY);
+    expect(payload).toEqual({
+      iat: expect.any(Number),
+      username: "test",
+      isAdmin: true,
+    });
+  });
+
+  test("Defaults to non-admin status if not provided", function () {
+    // given the security risk if this didn't work, checking this specifically
+    const token = createToken({ username: "test" });
+    const payload = jwt.verify(token, SECRET_KEY);
+    expect(payload).toEqual({
+      iat: expect.any(Number),
+      username: "test",
+      isAdmin: false,
+    });
+  });
+});
